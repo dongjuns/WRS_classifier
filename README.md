64dbd5371c7e7ff8c29688b9a8af7fff3e48902d
dongjuns <uinnaluv@naver.com>
2020-03-05 13:29:46 +0100

    Update: classifier.py and rename.py

--- README.md
+++ README.md
@@ -2,12 +2,24 @@
 
 The Model to classify the two classes, one is correctly assembled and other one has not the motor pulley.
 Firstly, I made a simulation environment using Blender.
+- The difference of between them is the existence of the motor pulley
+And it is exported and then used with Rasmus's synth-ml randomization tool.
 
-Modified conditions
-1. Camera information: focal length, sensor width size
-2. Existence of the motor pulley
+1. Camera information: focal length 8.5mm, sensor width size 9.2mm
+2. The motor pully is located on the motor, and it has some range, -1 mm < x < +1 mm.
 
 Training samples: 1200 wrs images with motor pulley, 1200 wrs images without motor pulley
 1200 samples: 600 images with noise + 600 images without noise
 
-MobileNet will be used to classify, and this is going to be the localization.
+MobileNet will be used to classify.
+
+0302: 0.6K datasets
+0303: 2K datasets
+0305: 5K datasets
+
+
+To do
+- Generating much more datasets
+- Customizing mobilenet
+- Hyper parameter optimization (maybe Bayesian)
+- Classifying unseen image
--- rename.py
+++ rename.py
@@ -1,11 +1,24 @@
+#!/usr/bin/env python3
 import os
+from datetime import datetime
+today = datetime.today().strftime('%m%d')
 
-filePath = '/home/dongjun/djplace/n22'
-fileNames = os.listdir(filePath)
-i = 0
-for name in fileNames:
-    src = os.path.join(filePath, name)
-    dst = os.path.basename(filePath) + '_' + str(i) + '.png'
-    dst = os.path.join(filePath, dst)
-    os.rename(src, dst)
-    i += 1
+def rename():
+    filePath = input("Enter the path of the directory: i.e.(case1/cycles or case1/cycles_denoise) \n")
+    #filePath = '/home/dongjun/djplace/synth-ml/examples/hello_world/synth_ml_data/case1/cycles'
+    filePath = '/home/dongjun/djplace/synth-ml/examples/hello_world/synth_ml_data/{}'.format(filePath)
+    fileNames = os.listdir(filePath)
+    i = 0
+    for name in fileNames:
+        src = os.path.join(filePath, name)
+        dst = os.path.basename(filePath) + '_' + today + '_' + str(i) + '.png'
+        dst = os.path.join(filePath, dst)
+        os.rename(src, dst)
+        i += 1
+
+try:
+    rename()
+except Exception as e:
+    print(e)
+finally:
+    print("Finished.")
